392a8f398c0b212dd6d874c67b89a31a7c7af9f8
Mark <wei.xiaoma@gmail.com>
2013-12-08 21:29:51 -0800

    randomly generates notes

--- app/scripts/controllers/questions.js
+++ app/scripts/controllers/questions.js
@@ -5,8 +5,13 @@ angular.module('eartrainingApp')
     $scope.currentOption = null;
     $scope.options = ["major 3rd", "fifth", "fourth", "octave"];
     $scope.notes = ["A4.mp3", "Ab4.mp3", "B4.mp3", "Bb4.mp3", "C4.mp3", "D4.mp3"];
-    $scope.randomNote = function() {
-      return $scope.notes[0];
+    $scope.randomNoteIndex = function() {
+      var index = Math.floor(Math.random() * $scope.notes.length);
+      return index;
     };
-
+    $scope.index1 = $scope.randomNoteIndex();
+    $scope.index2 = $scope.randomNoteIndex();
+    $scope.interval = $scope.index2 - $scope.index1;
+    $scope.noteUrl1 = 'piano/' + $scope.notes[$scope.index1];
+    $scope.noteUrl2 = 'piano/' + $scope.notes[$scope.index2];
   });
