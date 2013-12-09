9fd2d59b572af7b7fdd06c24b9138cd8e4c85ded
Mark <wei.xiaoma@gmail.com>
2013-12-08 21:57:05 -0800

    uses interval names

--- app/scripts/controllers/questions.js
+++ app/scripts/controllers/questions.js
@@ -4,7 +4,9 @@ angular.module('eartrainingApp')
   .controller('QuestionsCtrl', function ($scope) {
     $scope.currentOption = null;
     $scope.options = ["major 3rd", "fifth", "fourth", "octave"];
-    $scope.notes = ["A4.mp3", "Ab4.mp3", "B4.mp3", "Bb4.mp3", "C4.mp3", "D4.mp3"];
+    $scope.notes = ["A4", "Ab4", "B4", "Bb4", "C4", "D4", "Db4", "E4", "Eb4", "F4", "G4", "Gb4"];
+    $scope.stepNames = ['perfect unison', 'minor 2nd', 'major 2nd', 'minor 3rd', 'major 3rd',
+      'perfect 4th', 'tri-tone', 'perfect 5th', 'minor 6th', 'major 6th', 'minor 7th', 'major 7th', 'octave'];
     $scope.randomNoteIndex = function() {
       var index = Math.floor(Math.random() * $scope.notes.length);
       return index;
@@ -12,6 +14,9 @@ angular.module('eartrainingApp')
     $scope.index1 = $scope.randomNoteIndex();
     $scope.index2 = $scope.randomNoteIndex();
     $scope.interval = $scope.index2 - $scope.index1;
-    $scope.noteUrl1 = 'piano/' + $scope.notes[$scope.index1];
-    $scope.noteUrl2 = 'piano/' + $scope.notes[$scope.index2];
+    $scope.direction = ($scope.interval > 0) ? "an ascending " : "a descending ";
+    $scope.intervalName = $scope.direction + $scope.stepNames[Math.abs($scope.interval)];
+    $scope.noteUrl1 = 'piano/' + $scope.notes[$scope.index1] + '.mp3';
+    $scope.noteUrl2 = 'piano/' + $scope.notes[$scope.index2] + '.mp3';
+
   });
